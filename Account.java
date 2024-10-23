4686fc681964c191281d7db0f215fabeb25b18f0
KCarter336 <168095847+KCarter336@users.noreply.github.com>
2024-10-23 14:35:54 -0400

    Testing

---
+++ Account.java
@@ -0,0 +1,21 @@
+/* File: Account.java
+ * Author: Keegan Carter
+ * Purpose:
+ */
+
+public class Account {
+
+    private String lastName;
+    private String firstName;
+    private String sex;
+    private int DOB;
+    private int SSN;
+    private int accountNum;
+    private String accountType;
+    private int person;
+    private double balance;
+    private double costOfHouse;
+
+
+
+}
