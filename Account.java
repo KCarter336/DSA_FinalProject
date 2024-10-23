041fe0723548fff6e8e0994c383c75513b07f6b7
KCarter336 <168095847+KCarter336@users.noreply.github.com>
2024-10-23 15:00:47 -0400

    Further testing

--- Account.java
+++ Account.java
@@ -3,6 +3,8 @@
  * Purpose:
  */
 
+import java.util.Random;
+
 public class Account {
 
     private String lastName;
@@ -16,6 +18,43 @@ public class Account {
     private double balance;
     private double costOfHouse;
 
+    public Account(String accountType, int person){
+        this.accountType = accountType;
+        this.person = person;
+
+        private int getSSN() {
+            Random Generator = new Random();
+            SSN = Generator.nextInt(100000000, 999999999)
+            return SSN}
+
+        private int getAccountNum() {
+            Random Generator = new Random();
+            accountNum = Generator.nextInt(100000000, 999999999)
+            return accountNum}
+    }
+    public Account(String accountType, int person, double balance){
+
+        Private void setBalance(double balance){
+            This.balance = balance;
+
+        }
+
+// Loan account constructor
+        Public Account(String accountType, int person, double costOfHouse){
+            This.accountType = accountType
+            This.person = person
+            This.costOfHouse = costOfHouse
+        }
+
+// Methods related to loan accounts
+        Public double mortgage - input cost of house
+        Return: cost with interest
+        Public double mortgageDownPayment - input cost with interest
+        Return: down payment
+        Public double monthlyMortgagePayment - input cost with interest
+        Return: monthly payment
 
 
-}
+
+
+    }
