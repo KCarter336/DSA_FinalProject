e11636f2da74441dfec6a7da2303fddf9aaef1ba
KCarter336 <168095847+KCarter336@users.noreply.github.com>
2024-10-23 16:46:23 -0400

    Framework for driver

--- Account.java
+++ Account.java
@@ -1,69 +1,69 @@
-/* File: Account.java
- * Author: Keegan Carter
- * Purpose:
- */
-
-import java.util.Random;
-
-public class Account {
-
-    private String lastName;
-    private String firstName;
-    private String sex;
-    private int DOB;
-    private int SSN;
-    private int accountNum;
-    private String accountType;
-    private int person;
-    private double balance;
-    private double costOfHouse;
-
-    // Random account constructor
-    public Account(String accountType, int person) {
-        this.accountType = accountType;
-        this.person = person;
-    }
-
-    // Specified account constructor
-    public Account(String accountType, int person, double balance) {
-        private void setBalance(balance);
-        this.balance = balance;
-    }
-
-    // Loan account constructor
-    public Account (String accountType,int person, double costOfHouse){
-        this.accountType = accountType;
-        this.person = person;
-        this.costOfHouse = costOfHouse;
-    }
-
-
-    private int getSSN () {
-        Random Generator = new Random();
-        SSN = Generator.nextInt(100000000, 999999999);
-        return SSN;
-    }
-
-    private int getAccountNum () {
-        Random Generator = new Random();
-        accountNum = Generator.nextInt(100000000, 999999999);
-        return accountNum;
-    }
-
-
-
-
-// Methods related to loan accounts
-        Public double mortgage -input cost of house
-        Return:
-        cost with interest
-        Public double mortgageDownPayment -input cost with interest
-        Return:
-        down payment
-        Public double monthlyMortgagePayment -input cost with interest
-        Return:
-        monthly payment
-
-
-    }
-}
+///* File: Account.java
+// * Author: Keegan Carter
+// * Purpose:
+// */
+//
+//import java.util.Random;
+//
+//public class Account {
+//
+//    private String lastName;
+//    private String firstName;
+//    private String sex;
+//    private int DOB;
+//    private int SSN;
+//    private int accountNum;
+//    private String accountType;
+//    private int person;
+//    private double balance;
+//    private double costOfHouse;
+//
+//    // Random account constructor
+//    public Account(String accountType, int person) {
+//        this.accountType = accountType;
+//        this.person = person;
+//    }
+//
+//    // Specified account constructor
+//    public Account(String accountType, int person, double balance) {
+//        private void setBalance(balance);
+//        this.balance = balance;
+//    }
+//
+//    // Loan account constructor
+//    public Account (String accountType,int person, double costOfHouse){
+//        this.accountType = accountType;
+//        this.person = person;
+//        this.costOfHouse = costOfHouse;
+//    }
+//
+//
+//    private int getSSN () {
+//        Random Generator = new Random();
+//        SSN = Generator.nextInt(100000000, 999999999);
+//        return SSN;
+//    }
+//
+//    private int getAccountNum () {
+//        Random Generator = new Random();
+//        accountNum = Generator.nextInt(100000000, 999999999);
+//        return accountNum;
+//    }
+//
+//
+//
+//
+//// Methods related to loan accounts
+//        Public double mortgage -input cost of house
+//        Return:
+//        cost with interest
+//        Public double mortgageDownPayment -input cost with interest
+//        Return:
+//        down payment
+//        Public double monthlyMortgagePayment -input cost with interest
+//        Return:
+//        monthly payment
+//
+//
+//    }
+//}
--- Driver.java
+++ Driver.java
@@ -3,10 +3,52 @@
  * Purpose:
  */
 
+import java.util.Scanner;
+
 public class Driver {
 
     public static void main(String[] args){
-        
+
+        boolean go = true;
+
+        while (go){
+            System.out.println(
+                 "Menu: " +
+                         "\n Display all accounts (1) " +
+                         "\n Deposit to an account (2) " +
+                         "\n Withdrawal from an account (3) " +
+                         "\n Add interest to all accounts (4)" +
+                         "\n Total number of accounts (5)" +
+                         "\n Exit (6)" +
+                         "\n Enter selection here: "
+            );
+            Scanner cin = new Scanner(System.in);
+            int selection = cin.nextInt();
+            cin.nextLine();
+            switch (selection){
+                case 1:
+                    // print all account.name and account.number
+                case 2:
+                    // require input of account holder's last name
+                    // that account.deposit
+                case 3:
+                    // require input of account holder's last name
+                    // that account.withdrawal
+                    // needs a method to notify user if their balance is insufficient
+                        // ' "Sorry, your balance is less " + balance. '
+                case 4:
+                    // all accounts.addInterest
+                case 5:
+                    // print total number of accounts
+                        // number of savings accounts
+                        // number of checking accounts
+                        // number of student accounts
+                        // number of employee accounts
+
+            }
+
+        }
+
     }
 
 }
