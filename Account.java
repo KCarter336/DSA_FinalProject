bf2d029299cdde9fbd8c0026b51fb9812ced4762
KCarter336 <168095847+KCarter336@users.noreply.github.com>
2024-10-23 17:15:32 -0400

    Framework for Account class

--- Account.java
+++ Account.java
@@ -22,48 +22,44 @@ public class Account {
     public Account(String accountType, int person) {
         this.accountType = accountType;
         this.person = person;
+        this.accountNum = this.randAccountNum();
+        this.SSN = this.randSSN();
     }
-
     // Specified account constructor
     public Account(String accountType, int person, double balance) {
-        private void setBalance(balance);
+        this.accountType = accountType;
+        this.person = person;
         this.balance = balance;
     }
-
     // Loan account constructor
-    public Account (String accountType,int person, double costOfHouse){
+    public Account(String accountType, int person, double costOfHouse) {
         this.accountType = accountType;
         this.person = person;
         this.costOfHouse = costOfHouse;
     }
-
-
-    private int getSSN () {
+    private int randSSN() {
         Random Generator = new Random();
         SSN = Generator.nextInt(100000000, 999999999);
         return SSN;
     }
-
-    private int getAccountNum () {
+    private int randAccountNum() {
         Random Generator = new Random();
         accountNum = Generator.nextInt(100000000, 999999999);
         return accountNum;
     }
-
-
-
-
-// Methods related to loan accounts
-        Public double mortgage -input cost of house
-        Return:
-        cost with interest
-        Public double mortgageDownPayment -input cost with interest
-        Return:
-        down payment
-        Public double monthlyMortgagePayment -input cost with interest
-        Return:
-        monthly payment
-
-
+    public double mortgage(double costOfHouse) {
+        double mortgage = (costOfHouse - mortgageDownPayment(costOfHouse)) * 1.06;
+        return mortgage;
     }
+    public double mortgageDownPayment(double costOfHouse){
+        double downPayment = costOfHouse * 0.2;
+        return downPayment;
+    }
+    public double monthlyPayment(double mortgage){
+        double monthlyPayment = mortgage / (20 * 12);
+        return monthlyPayment;
+    }
+
 }
+
+
