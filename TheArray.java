93d395e13d2204508b4ca678d5e9c9cc6c8bf345
KCarter336 <168095847+KCarter336@users.noreply.github.com>
2024-11-11 15:31:39 -0500

    Homemade dynamically resizing array complete

--- TheArray.java
+++ TheArray.java
@@ -1,47 +1,45 @@
-///* File: TheArray.java
-// * Author: Keegan Carter
-// * Purpose:
-// */
-//
-//public class TheArray {
-//
-//    private Account[] accountList;
-//    private int startingSize;
-//    private int currentSize = accountList.length;
-//    private int numElements;
-//
-//
-//    public TheArray(int startingSize){
-//        this.startingSize = startingSize;
-//        accountList = new Account[startingSize];
-//    }
-//    public void countElements(){
-//        for(int i = 0; i < currentSize; i ++){
-//            if(accountList[i] != null){
-//                numElements ++;
-//            }
-//        }
-//    }
-//    public void increaseSize(){
-//        int increaseAmt = currentSize * 2;
-//        Account[] increaseList = Account[increaseAmt];
-//        for(int i = 0; i < currentSize; i ++){
-//            increaseList[i] = accountList[i];
-//        }
-//        this.accountList = increaseList;
-//    }
-//    public void addAccount(Account account){
-//        this.countElements();
-//        if(numElements < currentSize){
-//            accountList[numElements + 1] = account;
-//        }
-//        else{
-//            this.increaseSize();
-//            accountList[numElements + 1] = account;
-//        }
-//    }
-//    // getters
-//    public int getCurrentSize(){
-//        return this.currentSize;
-//    }
-//}
+/* File: TheArray.java
+ * Author: Keegan Carter
+ * Purpose:
+ */
+
+public class TheArray {
+
+    private Account[] accountList;
+    private int numElements;
+
+
+    public TheArray(){
+        this.accountList = new Account[3];
+    }
+    public void countElements(){
+        numElements = 0;
+        for(int i = 0; i < accountList.length; i ++){
+            if(accountList[i] != null){
+                numElements ++;
+            }
+        }
+    }
+    public void increaseSize(){
+        Account[] increaseList = new Account[accountList.length * 2];
+        for(int i = 0; i < accountList.length; i ++){
+            increaseList[i] = accountList[i];
+        }
+        accountList = increaseList;
+    }
+    public void addAccount(Account account){
+        this.countElements();
+        if(numElements < accountList.length -1 ){
+            accountList[numElements + 1] = account;
+        }
+        else{
+            this.increaseSize();
+            accountList[numElements + 1] = account;
+        }
+    }
+    // getters
+    public int getCurrentSize(){
+        return accountList.length;
+    }
+
+}
--- TheArrayTester.java
+++ TheArrayTester.java
@@ -1,30 +1,37 @@
-///* File: TheArrayTester.java
-// * Author: Keegan Carter
-// * Purpose:
-// */
-//
-//public class TheArrayTester {
-//
-//    public static void main(String[] args){
-//
-//        Account one = new Account();
-//        Account two = new Account();
-//        Account three = new Account();
-//        Account four = new Account();
-//
-//
-//        TheArray myArray = new TheArray(2);
-//        myArray.addAccount(one);
-//        System.out.println(myArray.getCurrentSize());
-//        myArray.addAccount(two);
-//        System.out.println(myArray.getCurrentSize());
-//        myArray.addAccount(three);
-//        System.out.println(myArray.getCurrentSize());
-//        myArray.addAccount(four);
-//        System.out.println(myArray.getCurrentSize());
-//
-//
-//
-//    }
-//
-//}
+/* File: TheArrayTester.java
+ * Author: Keegan Carter
+ * Purpose:
+ */
+
+public class TheArrayTester {
+
+    public static void main(String[] args){
+
+        Account one = new Account("c", 1, "Washington");
+        Account two = new Account("c", 1, "Adams");
+        Account three = new Account("c", 1, "Jefferson");
+        Account four = new Account("c", 1, "Madison");
+        Account five = new Account("c", 1, "Monroe");
+        Account six = new Account("c", 1, "Jackson");
+        Account seven = new Account("c", 1, "VanBuren");
+
+
+        TheArray myArray = new TheArray();
+        myArray.addAccount(one);
+        System.out.println(myArray.getCurrentSize());
+        myArray.addAccount(two);
+        System.out.println(myArray.getCurrentSize());
+        myArray.addAccount(three);
+        System.out.println(myArray.getCurrentSize());
+        myArray.addAccount(four);
+        System.out.println(myArray.getCurrentSize());
+        myArray.addAccount(five);
+        System.out.println(myArray.getCurrentSize());
+        myArray.addAccount(six);
+        System.out.println(myArray.getCurrentSize());
+        myArray.addAccount(seven);
+        System.out.println(myArray.getCurrentSize());
+
+    }
+
+}
